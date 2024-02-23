98a519a260ebf05a3abe23e84924f436643529cf
concefly <concefly@foxmail.com>
2024-02-23 13:32:33 +0800

    feat:  class Tpl

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,11 +1,14 @@
 import { tpl } from '../src';
+ 
 
 beforeEach(() => {
   tpl.files.clear();
+  tpl._compileCache.clear();
 });
 
 it('if-else-endif', () => {
-  const render = tpl.compile('a ${ _if`num === 1` }true${ _else }false${ _endif }');
+  tpl.files.set('test.txt', 'a ${ _if`num === 1` }true${ _else }false${ _endif }');
+  const render = tpl.compile('test.txt');
 
   const s1 = render({ num: 1 });
   const s2 = render({ num: 2 });
@@ -15,7 +18,8 @@ it('if-else-endif', () => {
 });
 
 it('if-elseif-else-endif', () => {
-  const render = tpl.compile('test ${ _if`num === 1` }A${ _elseif`num === 2` }B${ _else }C${ _endif }');
+  tpl.files.set('test.txt', 'test ${ _if`num === 1` }A${ _elseif`num === 2` }B${ _else }C${ _endif }');
+  const render = tpl.compile('test.txt');
 
   const s1 = render({ num: 1 });
   const s2 = render({ num: 2 });
@@ -27,20 +31,23 @@ it('if-elseif-else-endif', () => {
 });
 
 it('for-endfor', () => {
-  const render = tpl.compile('a ${ _for`index, name in names` }n-${_`name`}, ${ _endfor }');
+  tpl.files.set('test.txt', 'a ${ _for`index, name in names` }n-${_`name`}, ${ _endfor }');
+  const render = tpl.compile('test.txt');
   const s1 = render({ names: ['a', 'b', 'c'] });
   expect(s1).toEqual('a n-a, n-b, n-c, ');
 });
 
 it('print', () => {
-  const s1 = tpl.compile('nick:${_`name`}, age:${_`age`}, next_age:${_`age + 1`}, say-hi:${_`"hi" + name`}')({ name: 'Jam', age: 18 });
+  tpl.files.set('test.txt', 'nick:${_`name`}, age:${_`age`}, next_age:${_`age + 1`}, say-hi:${_`"hi" + name`}');
+  const s1 = tpl.compile('test.txt')({ name: 'Jam', age: 18 });
   expect(s1).toEqual('nick:Jam, age:18, next_age:19, say-hi:hiJam');
 });
 
 it('include', () => {
   tpl.files.set('logo.txt', 'LOGO');
   tpl.files.set('header.txt', '[THIS IS HEADER, title=${ _`title` }, logo=${ _include`logo.txt` }]');
+  tpl.files.set('test.txt', '${ _include`header.txt { title }` }-b, ${ _`desc` }');
 
-  const s1 = tpl.compile('${ _include`header.txt { title }` }-b, ${ _`desc` }')({ title: 'Dog', desc: 'dog site' });
+  const s1 = tpl.compile('test.txt')({ title: 'Dog', desc: 'dog site' });
   expect(s1).toEqual('[THIS IS HEADER, title=Dog, logo=LOGO]-b, dog site');
 });
--- src/index.ts
+++ src/index.ts
@@ -63,81 +63,12 @@ export type ITplToken$ =
 
 export type ITplToken = ITplTokenLiteral | ITplToken$;
 
-export function tpl() {}
+export type ITplRender = (ctx: Record<string, any>) => string;
 
-tpl.files = new Map<string, string>();
+export class Tpl {
+  files = new Map<string, string>();
 
-tpl.tokenize = function (strings: TemplateStringsArray, ...datas: ITplToken$[]) {
-  const tokens: ITplToken[] = [];
-
-  for (let i = 0; i < strings.length; i++) {
-    const s = strings[i];
-    const d = datas[i];
-
-    tokens.push({ type: 'literal', source: s });
-    if (d) tokens.push(d);
-  }
-
-  return tokens;
-};
-
-tpl.$if = function (strings: TemplateStringsArray): ITplToken$If {
-  const expression = strings[0];
-  return { type: '$if', expression };
-};
-
-tpl.$else = { type: '$else' } satisfies ITplToken$Else;
-
-tpl.$elseif = function (strings: TemplateStringsArray): ITplToken$ElseIf {
-  const expression = strings[0];
-  return { type: '$elseif', expression };
-};
-
-tpl.$endif = { type: '$endif' } satisfies ITplToken$EndIf;
-
-tpl.$for = function (strings: TemplateStringsArray): ITplToken$For {
-  const expRaw = strings[0].trim();
-  const match = expRaw.match(/(.*?),\s*(.*?)\s+in\s+(.*)$/);
-  if (!match) throw new Error(`Invalid $for expression: ${expRaw}`);
-
-  const loopIndexSym = match[1].trim();
-  const loopValueSym = match[2].trim();
-  const valueSym = match[3].trim();
-
-  return { type: '$for', loopIndexSym, loopValueSym, valueSym };
-};
-
-tpl.$endfor = { type: '$endfor' } satisfies ITplToken$EndFor;
-
-tpl.$print = function (strings: TemplateStringsArray): ITplToken$Print {
-  const expression = strings[0];
-  return { type: '$print', expression };
-};
-
-tpl.$include = function (strings: TemplateStringsArray): ITplToken$Include {
-  const expRaw = strings[0].trim();
-
-  let path: string;
-  let expression: string | undefined;
-
-  if (expRaw.indexOf(' ') > 0) {
-    const match = expRaw.match(/(.*?)\s+(.*)$/);
-    if (!match) throw new Error(`Invalid $include expression: ${expRaw}`);
-
-    path = match[1].trim();
-    expression = match[2].trim();
-  } else {
-    path = expRaw;
-    expression = undefined;
-  }
-
-  return { type: '$include', path, expression };
-};
-
-tpl._runtime = {
-  _includeCache: new Map<string, Function>(),
-
-  iter: function (data: any) {
+  _rt_iter(data: any) {
     if (Array.isArray(data)) {
       return {
         [Symbol.iterator]: function* () {
@@ -159,117 +90,196 @@ tpl._runtime = {
     }
 
     return data;
-  },
-  include: function (path: string, data: any) {
-    const content = tpl.files.get(path);
-    if (!content) throw new Error(`File not found: ${path}`);
+  }
 
-    const render = tpl._runtime._includeCache.get(path) || tpl.compile(content);
+  _rt_include(path: string, data: any) {
+    const render = this.compile(path);
     return render(data);
-  },
-};
+  }
 
-tpl.makeRender = function (tokens: ITplToken[]) {
-  const _retSym = '__$$ret';
+  tokenize(strings: TemplateStringsArray, ...datas: ITplToken$[]) {
+    const tokens: ITplToken[] = [];
 
-  let script: string = `
+    for (let i = 0; i < strings.length; i++) {
+      const s = strings[i];
+      const d = datas[i];
+
+      tokens.push({ type: 'literal', source: s });
+      if (d) tokens.push(d);
+    }
+
+    return tokens;
+  }
+
+  $if(strings: TemplateStringsArray): ITplToken$If {
+    const expression = strings[0];
+    return { type: '$if', expression };
+  }
+
+  $else = { type: '$else' } satisfies ITplToken$Else;
+
+  $elseif(strings: TemplateStringsArray): ITplToken$ElseIf {
+    const expression = strings[0];
+    return { type: '$elseif', expression };
+  }
+
+  $endif = { type: '$endif' } satisfies ITplToken$EndIf;
+
+  $for(strings: TemplateStringsArray): ITplToken$For {
+    const expRaw = strings[0].trim();
+    const match = expRaw.match(/(.*?),\s*(.*?)\s+in\s+(.*)$/);
+    if (!match) throw new Error(`Invalid $for expression: ${expRaw}`);
+
+    const loopIndexSym = match[1].trim();
+    const loopValueSym = match[2].trim();
+    const valueSym = match[3].trim();
+
+    return { type: '$for', loopIndexSym, loopValueSym, valueSym };
+  }
+
+  $endfor = { type: '$endfor' } satisfies ITplToken$EndFor;
+
+  $print(strings: TemplateStringsArray): ITplToken$Print {
+    const expression = strings[0];
+    return { type: '$print', expression };
+  }
+
+  $include(strings: TemplateStringsArray): ITplToken$Include {
+    const expRaw = strings[0].trim();
+
+    let path: string;
+    let expression: string | undefined;
+
+    if (expRaw.indexOf(' ') > 0) {
+      const match = expRaw.match(/(.*?)\s+(.*)$/);
+      if (!match) throw new Error(`Invalid $include expression: ${expRaw}`);
+
+      path = match[1].trim();
+      expression = match[2].trim();
+    } else {
+      path = expRaw;
+      expression = undefined;
+    }
+
+    return { type: '$include', path, expression };
+  }
+
+  makeRender(tokens: ITplToken[]): ITplRender {
+    const _retSym = '__$$ret';
+
+    let script: string = `
 if (typeof ctx !== 'object' || ctx === null) throw new Error('ctx must be an object');
 
 let ${_retSym} = '';
 with(ctx) {
 `;
-  let _t = 1;
+    let _t = 1;
 
-  const _indent = () => '  '.repeat(_t);
+    const _indent = () => '  '.repeat(_t);
 
-  for (let i = 0; i < tokens.length; i++) {
-    const token = tokens[i];
+    for (let i = 0; i < tokens.length; i++) {
+      const token = tokens[i];
 
-    switch (token.type) {
-      case 'literal':
-        script += _indent() + `${_retSym} += "${token.source}";\n`;
-        break;
+      switch (token.type) {
+        case 'literal':
+          script += _indent() + `${_retSym} += "${token.source}";\n`;
+          break;
 
-      case '$if':
-        script += _indent() + `if (${token.expression}) {\n`;
-        _t += 1;
-        break;
+        case '$if':
+          script += _indent() + `if (${token.expression}) {\n`;
+          _t += 1;
+          break;
 
-      case '$elseif':
-        _t -= 1;
-        script += _indent() + `} else if (${token.expression}) {\n`;
-        _t += 1;
-        break;
+        case '$elseif':
+          _t -= 1;
+          script += _indent() + `} else if (${token.expression}) {\n`;
+          _t += 1;
+          break;
 
-      case '$else':
-        _t -= 1;
-        script += _indent() + `} else {\n`;
-        _t += 1;
-        break;
+        case '$else':
+          _t -= 1;
+          script += _indent() + `} else {\n`;
+          _t += 1;
+          break;
 
-      case '$endif':
-        _t -= 1;
-        script += _indent() + `}\n`;
-        break;
+        case '$endif':
+          _t -= 1;
+          script += _indent() + `}\n`;
+          break;
 
-      case '$for':
-        script += _indent() + `for (const [${token.loopIndexSym}, ${token.loopValueSym}] of this._runtime.iter(${token.valueSym})) {\n`;
-        _t += 1;
-        break;
+        case '$for':
+          script += _indent() + `for (const [${token.loopIndexSym}, ${token.loopValueSym}] of this._rt_iter(${token.valueSym})) {\n`;
+          _t += 1;
+          break;
 
-      case '$endfor':
-        _t -= 1;
-        script += _indent() + `}\n`;
-        break;
+        case '$endfor':
+          _t -= 1;
+          script += _indent() + `}\n`;
+          break;
 
-      case '$print':
-        script += _indent() + `${_retSym} += String(${token.expression});\n`;
-        break;
+        case '$print':
+          script += _indent() + `${_retSym} += String(${token.expression});\n`;
+          break;
 
-      case '$include':
-        script += _indent() + `${_retSym} += this._runtime.include("${token.path}", ${token.expression ?? '{}'});\n`;
-        break;
+        case '$include':
+          script += _indent() + `${_retSym} += this._rt_include("${token.path}", ${token.expression ?? '{}'});\n`;
+          break;
 
-      default:
-        throw new Error('unknown token type: ' + (token as any).type);
+        default:
+          throw new Error('unknown token type: ' + (token as any).type);
+      }
     }
+
+    _t -= 1;
+    script += _indent() + `}\n`;
+    script += _indent() + `return ${_retSym};\n`;
+
+    const render = new Function('ctx', script).bind(this) as ITplRender;
+    return render;
   }
 
-  _t -= 1;
-  script += _indent() + `}\n`;
-  script += _indent() + `return ${_retSym};\n`;
+  _compileCache = new Map<string, ITplRender>();
 
-  const render = new Function('ctx', script).bind(this);
-  return render;
-};
+  compile(path: string): ITplRender {
+    const input = this.files.get(path);
+    if (!input) throw new Error(`File not found: ${path}`);
 
-tpl.compile = function (input: string) {
-  const _wrapper = new Function(
-    '_tokenize',
-    '_if',
-    '_elseif',
-    '_else',
-    '_endif',
-    '_for',
-    '_endfor',
-    '_',
-    '_include',
-    // body
-    `return _tokenize\`${input}\`;`
-  );
+    // check cache
+    if (this._compileCache.has(path)) return this._compileCache.get(path)!;
 
-  const tokens: ITplToken[] = _wrapper(
-    tpl.tokenize,
-    tpl.$if,
-    tpl.$elseif,
-    tpl.$else,
-    tpl.$endif,
-    tpl.$for,
-    tpl.$endfor,
-    tpl.$print,
-    tpl.$include
-  );
+    const _wrapper = new Function(
+      '_tokenize',
+      '_if',
+      '_elseif',
+      '_else',
+      '_endif',
+      '_for',
+      '_endfor',
+      '_',
+      '_include',
+      // body
+      `return _tokenize\`${input}\`;`
+    );
 
-  const render = tpl.makeRender(tokens);
-  return render;
-};
+    const tokens: ITplToken[] = _wrapper(
+      this.tokenize,
+      this.$if,
+      this.$elseif,
+      this.$else,
+      this.$endif,
+      this.$for,
+      this.$endfor,
+      this.$print,
+      this.$include
+    );
+
+    const render = this.makeRender(tokens);
+
+    // cache
+    this._compileCache.set(path, render);
+
+    return render;
+  }
+}
+
+export const tpl = new Tpl();
